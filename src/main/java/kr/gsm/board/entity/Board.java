d4f1f5b4785314494b8c7488ded318eaca5d7b90
bitcocom <bitcocom@empas.com>
2025-03-11 11:30:21 +0900

    spring

--- src/main/java/kr/gsm/board/entity/Board.java
+++ src/main/java/kr/gsm/board/entity/Board.java
@@ -1,7 +1,19 @@
 package kr.gsm.board.entity;
+
+import jakarta.persistence.*;
+
 // Board(Object)---mapping-->DB Table
 // ORM
 // Spring JPA API
 @Entity
-public class Board {
+public class Board { // 번호(PK), 제목, 내용, 작성자....
+    @Id // PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long num; // null
+
+    @Column(name = "title", length=500, nullable = false, unique = true )
+    private String title; // varchar(500)
+
+    private String content;
+    private String name; // name
 }
