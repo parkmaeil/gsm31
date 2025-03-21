77d245a368e3aa3028104bfde13d427a589ced69
bitcocom <bitcocom@empas.com>
2025-03-21 11:57:02 +0900

    spring

--- src/main/java/kr/gsm/board/repository/BoardRepository.java
+++ src/main/java/kr/gsm/board/repository/BoardRepository.java
@@ -12,23 +12,25 @@ import java.util.Optional;
 // Spring Data JPA
 @Repository
 public interface BoardRepository extends JpaRepository<Board,Long> {
-    // 1. 상속받은 메서드를 사용하는 방법
+    // 1. 상속받은 메서드를 사용하는 방법(CRUD)
     //public List<Board> findAll(); --> Hibernate엔진 SQL생성
     // findAll() --> select * from Board
     // save(Board board)
     // findById(Long id) -> select * from Board where id=?
     // deleteById()
-    // 2. 규칙이 있는 메서드를 사용하는 방법(select) - Query Method
+    // 2. 규칙이 있는 메서드를 사용하는 방법(select) <- Query Method
     // findBy+객체의 속성이름
     // public Optional<Board> getByTitle(String title);
     // public Optional<Board> getByTitle(String title);
     // 3. JPQL(사용자 정의 쿼리)
     // - Entity를 기준으로 SQL을 작성
-    //@Query("select b from Board b where b.title=?1")
-    //public Optional<Board> getTitle(String title);
-    // - Table을 기준으로 SQL을 작성
-    @Query(value = "select * from board where title=?1", nativeQuery = true)
+    @Query("select b from Board b where b.title=?1")
     public Optional<Board> getTitle(String title);
+    // - Table을 기준으로 SQL을 작성
+    //@Query(value = "select * from board where title=?1", nativeQuery = true)
+    //public Optional<Board> getTitle(String title);
+
+    // 4. Querydsl 방법(SQL+Method 병렬 체이닝) - 후반
 }
 // public class EntityManager implements BoardRepository{
 
