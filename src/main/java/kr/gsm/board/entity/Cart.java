a16b173e6d8396582013defda36116efbcce160a
bitcocom <bitcocom@empas.com>
2025-03-25 10:52:59 +0900

    spring

--- src/main/java/kr/gsm/board/entity/Cart.java
+++ src/main/java/kr/gsm/board/entity/Cart.java
@@ -1,4 +1,32 @@
 package kr.gsm.board.entity;
 
-public class Cart {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+public class Cart { // 장바구니(구매)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int quantity; // 수량
+    private LocalDateTime cartDate;// 구매일자
+
+    @PrePersist
+    public void onCreate(){
+        this.cartDate=LocalDateTime.now();
+    }
+    // 구매(N) - 고객(1) 관계
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    // 구매(N) - 책(1) 관계
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
 }
