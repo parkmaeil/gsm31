d92f769759410f5002dd68b8ca68d98c8d55cb85
bitcocom <bitcocom@empas.com>
2025-03-25 10:59:38 +0900

    spring

--- src/main/java/kr/gsm/board/entity/Book.java
+++ src/main/java/kr/gsm/board/entity/Book.java
@@ -22,4 +22,8 @@ public class Book {
     // 책 - 리뷰의 관계(1:1, N:1,1:N, M:N)
     @OneToMany(mappedBy ="book", cascade = CascadeType.ALL)
     private List<Review> reviews;
+
+    // 책 - 구매의 관계
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
+    private List<Cart> carts;
 }
--- src/main/java/kr/gsm/board/entity/Customer.java
+++ src/main/java/kr/gsm/board/entity/Customer.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-public class Customer { // 고객(1)-리뷰(N)
+public class Customer { // 고객(1)-리뷰(N) / 고객(1)-구매(N)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,4 +22,8 @@ public class Customer { // 고객(1)-리뷰(N)
     // 고객-리뷰의 관계
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
     private List<Review> reviews;
+
+    // 고객-구매의 관계
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Cart> carts;
 }
