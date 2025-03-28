b61d8ac4199f72ba591f6e9de20d689f258d7cd5
bitcocom <bitcocom@empas.com>
2025-03-28 12:09:19 +0900

    spring

--- src/main/java/kr/gsm/board/service/BookService.java
+++ src/main/java/kr/gsm/board/service/BookService.java
@@ -23,14 +23,15 @@ public class BookService {
     @Transactional(readOnly = true)
     public List<BookDTO> getAllLists(){
         // 순환참조문제 해결 ? BookDTO
-        List<Book> books=bookRepository.findAll();
+        List<Book> books=bookRepository.findAll(); // 1번SQL
         // 리뷰정보 가져오기 ?
         return books.stream().map((book)->{
             //List<Review> reviews=book.getReviews();//EntityManager를 접근
+            //                          N번의 SQL(성능떨어진다-->N+1 해결?, fetch join, @EntityGraph)
             List<ReviewDTO> reviewDTOS=book.getReviews().stream().map((review)->{
                return new ReviewDTO(review.getId(), review.getCost(), review.getContent(), review.getCreatedAt());
             }).toList();
             return new BookDTO(book.getId(),book.getTitle(),book.getPrice(),book.getAuthor(),book.getPage(),reviewDTOS);
         }).toList(); //-------->SQL
-    }
+    }// 끝나기 전까지 EntityManager를 Open
 }
